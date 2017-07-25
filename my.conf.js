08d660db11c04fe568fab40ed9156dc0e071e926
HD Haasbroek <u15046657@tuks.co.za>
2017-07-25 15:59:44 +0200

    #24 updated cofig

--- my.conf.js
+++ my.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'build/helloworld.js',
+      'build/*.js',
       'tests/*.js'
     ],
 
